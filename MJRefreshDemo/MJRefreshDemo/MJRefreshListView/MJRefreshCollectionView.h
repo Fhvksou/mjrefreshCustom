cdc772bb9745bb47ab0b8032d7e894d9fb5c1c8f
fhkvsou <497001857@qq.com>
2017-10-11 15:19:07 +0800

    fix bug

--- MJRefreshDemo/MJRefreshDemo/MJRefreshListView/MJRefreshCollectionView.h
+++ MJRefreshDemo/MJRefreshDemo/MJRefreshListView/MJRefreshCollectionView.h
@@ -48,6 +48,6 @@
 /** 当底部控件出现多少时就自动刷新(默认为1.0，也就是底部控件完全出现时，才会自动刷新) */
 @property (assign, nonatomic) CGFloat triggerAutomaticallyRefreshPercent;
 
-@property (nonatomic ,assign) id<MDRefreshCollectionViewDelegate> refreshDelegate;
+@property (nonatomic ,weak) id<MDRefreshCollectionViewDelegate> refreshDelegate;
 
 @end
